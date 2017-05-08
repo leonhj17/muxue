5988b4ff90e4dd36e5210044b46cbdce9eed9ecf
huangjian <huangjian17@outlook.com>
2017-05-08 11:13:28 +0800

    xadmin

--- organization/models.py
+++ organization/models.py
@@ -1,5 +1,41 @@
 from __future__ import unicode_literals
+from datetime import datetime
 
 from django.db import models
-
+from DjangoUeditor.models import UEditorField
 # Create your models here.
+
+
+class CityDict(models.Model):
+    name = models.CharField(max_length=20,verbose_name= u'城市')
+    desc = models.CharField(max_length=200,verbose_name=u'描述')
+    add_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = u'城市'
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.name
+
+
+class CourseOrg(models.Model):
+    name = models.CharField(max_length=50, verbose_name=u'机构名称')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
