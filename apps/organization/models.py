9cb74388f041c03ea45f8def5b71d63fd2703bc4
huangjian <huangjian17@outlook.com>
2017-06-08 09:01:00 +0800

    template inherit

--- apps/organization/models.py
+++ apps/organization/models.py
@@ -10,7 +10,7 @@ from DjangoUeditor.models import UEditorField
 class CityDict(models.Model):
     name = models.CharField(max_length=20,verbose_name= u'城市')
     desc = models.CharField(max_length=200,verbose_name=u'描述')
-    add_time = models.DateTimeField(default=datetime.now)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
 
     class Meta:
         verbose_name = u'城市'
@@ -34,7 +34,7 @@ class CourseOrg(models.Model):
     city = models.ForeignKey(CityDict,verbose_name=u'所在城市')
     students = models.IntegerField(default=0,verbose_name=u'学习人数')
     course_nums = models.IntegerField(default=0,verbose_name=u'课程数')
-    add_time = models.DateTimeField(default=datetime.now)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
 
     class Meta:
         verbose_name = u'课程机构'
@@ -59,7 +59,7 @@ class Teacher(models.Model):
     age = models.IntegerField(default=18,verbose_name=u'年龄')
     image = models.ImageField(default='',upload_to='teacher/%Y/%m',
                               verbose_name=u'头像',max_length=100)
-    add_time = models.DateTimeField(default=datetime.now)
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
 
     class Meta:
         verbose_name = u'教师'
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -1,3 +1,18 @@
+# _*_ encoding:utf-8 _*_
 from django.shortcuts import render
+from django.views.generic import View
+
+from .models import CityDict, CourseOrg
 
 # Create your views here.
+
+
+class OrgView(View):
+    def get(self, request):
+        all_city = CityDict.objects.all()
+        all_org = CourseOrg.objects.all()
+
+        return render(request, 'org-list.html', {
+            'all_city':all_city,
+            'all_org':all_org
+        })
